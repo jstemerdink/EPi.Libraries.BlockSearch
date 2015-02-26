80f5eda1476eadecb4191700fc38383447814de9
Jeroen <jeroen.developer@outlook.com>
2015-02-26 20:48:37 +0100

    Added new EPiServer Logmanager, bumped version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,6 +2,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+using EPiServer.Logging;
+
 using log4net.Config;
 
 // General Information about an assembly is controlled through the following 
@@ -37,7 +40,7 @@ using log4net.Config;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
-[assembly: XmlConfigurator(Watch = true)]
+[assembly: LoggerFactory(typeof(EPiServer.Logging.Log4Net.Log4NetLoggerFactory))]
