824771fb26b2e1dd2ae48654131e5e8491b9265e
Jeroen <jeroen.developer@outlook.com>
2016-09-14 14:30:42 +0200

    Updated to work with EPiServer 10

--- DataAnnotations/AdditionalSearchContentAttribute.cs
+++ DataAnnotations/AdditionalSearchContentAttribute.cs
@@ -1,5 +1,4 @@
-﻿// Copyright© 2015 Jeroen Stemerdink.
-// 
+﻿// Copyright © 2016 Jeroen Stemerdink.
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without
@@ -8,10 +7,8 @@
 // copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following
 // conditions:
-// 
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 // OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,19 +17,18 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-
-using System;
-
 namespace EPi.Libraries.BlockSearch.DataAnnotations
 {
+    using System;
+
     /// <summary>
-    ///     Class AdditionalSearchContentAttribute. This class cannot be inherited.
+    /// Class AdditionalSearchContentAttribute. This class cannot be inherited.
     /// </summary>
+    /// <seealso cref="System.Attribute" />
+    /// <author>Jeroen Stemerdink</author>
     [AttributeUsage(AttributeTargets.Property)]
     public sealed class AdditionalSearchContentAttribute : Attribute
     {
-        #region Public Properties
-
         /// <summary>
         ///     Gets a value indicating whether the property is used for additional search content.
         /// </summary>
@@ -44,7 +40,5 @@ namespace EPi.Libraries.BlockSearch.DataAnnotations
                 return true;
             }
         }
-
-        #endregion
     }
 }
